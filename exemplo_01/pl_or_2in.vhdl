ead9ed8d37d7123fdfc6918f8078c0b85c51961f
luizfelipefrosa <luizfelipefonsecarosa@gmail.com>
2021-12-03 08:40:07 -0300

    Primeiras portas

---
+++ exemplo_01/pl_or_2in.vhdl
@@ -0,0 +1,16 @@
+-- Entidade OR
+-- OR de duas entradas
+-- 03/12/2021
+
+-- Interface
+entity pl_or_2in is
+
+    port(
+        A: in bit;
+        B: in bit;
+        S: out bit
+    );
+
+end entity pl_or_2in;
+
+-- Comportamento
