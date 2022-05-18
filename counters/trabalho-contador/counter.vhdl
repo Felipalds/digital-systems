0bda4aaac5c845e13fe893a2f011cc27d12e5af3
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2022-05-18 20:37:37 -0300

    finishing counters

--- counters/trabalho-contador/counter.vhdl
+++ counters/trabalho-contador/counter.vhdl
@@ -3,14 +3,14 @@ use ieee.std_logic_1164.all;
 
 entity contador is
     port(
-        clk: in bit;
-        reset: in bit;
+        clk: in std_logic;
+        reset: in std_logic;
         q: out std_logic_vector(3 downto 0)
         );
 end entity;
 
 architecture cont of contador is
-    component ffjk is
+    component ffjk_process is
 	port(
 		j, k   : in  std_logic;
 		clock  : in  std_logic;
@@ -26,20 +26,20 @@ architecture cont of contador is
 
 begin 
 
-    u0: ffjk port map(sj(0), sk(0), clk, spr, reset, sq(0), snq(0));
-    u1: ffjk port map(sj(1), sk(1), clk, spr, reset, sq(1), snq(1));
-    u2: ffjk port map(sj(2), sk(2), clk, reset, spr, sq(2), snq(2));
-    u3: ffjk port map(sj(3), sk(3), clk, spr, reset, sq(3), snq(3));
+    u0: ffjk_process port map(sj(0), sk(0), clk, spr, reset, sq(0), snq(0));
+    u1: ffjk_process port map(sj(1), sk(1), clk, spr, reset, sq(1), snq(1));
+    u2: ffjk_process port map(sj(2), sk(2), clk, reset, spr, sq(2), snq(2));
+    u3: ffjk_process port map(sj(3), sk(3), clk, spr, reset, sq(3), snq(3));
 
-    sj0 <= snq(3) or sq(2);
-    sj1 <= ( snq(3) and sq(0) ) or ( sq(3) and sq(2) and snq(0) );
-    sj2 <= ( sq(3) and snq(0) ) or ( snq(3) and sq(1) and sq(0) );
-    sj3 <= ( sq(3) and sq(2) and snq(0) ) or ( snq(2) and sq(1) and sq(0) );
+    sj(0) <= snq(3) or sq(2);
+    sj(1) <= (snq(3) and sq(0)) or (sq(3) and sq(2) and snq(0));
+    sj(2) <= (sq(3) and snq(0)) or (snq(3) and sq(1) and sq(0));
+    sj(3) <= (sq(2) and sq(1) and snq(0)) or (snq(2) and sq(1) and sq(0));
 
-    sk0 <= snq(3) or sq(2) and snq(1);
-    sk1 <= ( snq(3) and sq(0) ) or ( sq(3) and snq(2) ) or ( sq(3) and sq(0) );
-    sk2 <= sq(3) and snq(1);
-    sk3 <= ( sq(2) and snq(1) and sq(0) ) or ( snq(2) and snq(0) );
+    sk(0) <= (snq(3) or sq(2) or snq(1));
+    sk(1) <= (snq(3) and sq(0)) or (sq(3) and snq(2)) or (sq(3) and snq(0));
+    sk(2) <= sq(3) and snq(1);
+    sk(3) <= (sq(2) and snq(1) and sq(0)) or (snq(2) and snq(0));
 
     q <= sq;
 
--- counters/trabalho-contador/tb_counter.vhdl
+++ counters/trabalho-contador/tb_counter.vhdl
@@ -2,7 +2,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity tb_counter is
-    -- entidade vazia
 end tb_counter;
 
 architecture test of tb_counter is
@@ -10,54 +9,36 @@ architecture test of tb_counter is
 
     component contador is
         port(
-            clk: in bit;
-            reset: in bit;
+            clk: in std_logic;
+            reset: in std_logic;
             q: out std_logic_vector(3 downto 0)
             );
     end component;
 
-    signal sq, snq : std_logic(3 downto 0);
+    signal sq, snq : std_logic_vector(3 downto 0);
     signal sreset : std_logic;
     signal sclk : std_logic := '1';
 
 begin
     -- instancia de JK e port map
-    u_ffjk : ffjk port map(sj, sk, sclk, spr, scl, sq, snq);
+    u_counter : contador port map(sclk, sreset, sq);
     -- process
     tbp : process
     begin
         -- passo 0
-        spr <= '1';
-        scl <= '0';
-        sj  <= '0';
-        sk  <= '0';
-        wait for CLK_PERIOD;
+        sclk <= not sclk;
+        wait for CLK_PERIOD/2;
+    end process;
 
-        -- desativação de clear
-        scl <= '1';
-        -- alteração de J e K
-        sj  <= '1';
-        sk  <= '1';
+    tbc : process
+    begin
+        sreset <= '0';
         wait for CLK_PERIOD;
-
-        -- passo 2
-        sj  <= '0';
-        sk  <= '1';
+        
+        sreset <= '1';
         wait for CLK_PERIOD;
-
-        -- passo 3
-        sj  <= '1';
-        sk  <= '0';
-        wait for CLK_PERIOD;
-
+       
         wait;
     end process;
 
-    -- process para Clock
-    tbc : process
-    begin
-        sclk <= not(sclk);
-        wait for CLK_PERIOD/2; 
-    end process;
-
-end architecture test;
+end architecture;
